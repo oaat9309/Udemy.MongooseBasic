4c8ad9755f2f84341d05574a73f1adcf29e37a4f
oaat9309 <kite1993@naver.com>
2021-12-18 20:23:47 +0900

    (12.18) Validator Update

--- product.js
+++ product.js
@@ -11,25 +11,53 @@ mongoose.connect('mongodb://localhost:27017/shopApp')
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        maxlength: 20
     },
     price: {
-        type: Number
+        type: Number,
+        required: true,
+        min: 0
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: [String],
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
     }
 })
 
 const Product = mongoose.model('Product', productSchema)
 
-const bike = new Product({
-    name: 'Mountain Bike', price: 599
-})
+// const bike = new Product({
+//     name: 'Tire Pump', price: 19.50, categories: ['Cycling', 'Safety']
+// })
 
-bike.save()
+// bike.save()
+//     .then(data => {
+//         console.log('it worked')
+//         console.log(data)
+//     })
+//     .catch(err => {
+//         console.log('error warning')
+//         console.log(err.errors.name.properties.message)
+//     })
+
+Product.findOneAndUpdate({name: 'Mountain Bike'}, {name: 'Tire Pump'}, {price: 12.50})
     .then(data => {
         console.log('it worked')
         console.log(data)
     })
     .catch(err => {
         console.log('error warning')
-        console.log(err.errors)
+        console.log(err.errors.name.properties.message)
     })
